703617b19d4f7afa08a9f515ef6b194ed02acd11
denisdulici <denisdulici@gmail.com>
2019-07-18 14:40:34 +0300

    tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, WithoutMiddleware;
 
     public $request;
 
