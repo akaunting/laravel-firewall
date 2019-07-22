0cc50c92c7adfc9ad0b0eabfc5b0aa303d51a016
denisdulici <denisdulici@gmail.com>
2019-07-22 23:24:17 +0300

    fixed email notification

--- src/Resources/lang/en/notifications.php
+++ src/Resources/lang/en/notifications.php
@@ -4,8 +4,8 @@ return [
 
     'mail' => [
 
-        'subject' => ':fire: Possible attack on :domain',
-        'message' => 'A possible :middleware attack on :domain has been detected from :ip address. The following URL has been affected:<br><br>:url<br><br>Regards',
+        'subject' => '🔥 Possible attack on :domain',
+        'message' => 'A possible :middleware attack on :domain has been detected from :ip address. The following URL has been affected: :url',
 
     ],
 
