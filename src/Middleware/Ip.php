924529c4ae1db454745aa59a2731679e8ff287b8
denisdulici <denisdulici@gmail.com>
2020-01-04 21:36:44 +0300

    check if table exists

--- src/Middleware/Ip.php
+++ src/Middleware/Ip.php
@@ -4,11 +4,16 @@ namespace Akaunting\Firewall\Middleware;
 
 use Akaunting\Firewall\Abstracts\Middleware;
 use Akaunting\Firewall\Models\Ip as Model;
+use Illuminate\Support\Facades\Schema;
 
 class Ip extends Middleware
 {
     public function check($patterns)
     {
+        if (!Schema::hasTable('firewall_ips')) {
+            return false;
+        }
+
         return Model::blocked($this->ip())->pluck('id')->first();
     }
 }
