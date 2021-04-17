5803f8166b98491feafb2367c8232361e800fea9
Denis Duliçi <denisdulici@gmail.com>
2021-04-17 11:14:42 +0300

    fixed #34

--- src/Listeners/NotifyUsers.php
+++ src/Listeners/NotifyUsers.php
@@ -3,10 +3,8 @@
 namespace Akaunting\Firewall\Listeners;
 
 use Akaunting\Firewall\Events\AttackDetected as Event;
-use Akaunting\Firewall\Notifications\AttackDetected as Notification;
-use Exception;
-use GuzzleHttp\Exception\RequestException;
-use Illuminate\Notifications\Notifiable;
+use Akaunting\Firewall\Notifications\AttackDetected;
+use Akaunting\Firewall\Notifications\Notifiable;
 use Throwable;
 
 class NotifyUsers
@@ -20,34 +18,10 @@ class NotifyUsers
      */
     public function handle(Event $event)
     {
-        $notifiable = $this->getNotifiableClass();
-
         try {
-            $notifiable->notify(new Notification($event->log));
-        } catch (Exception | RequestException | Throwable $e) {
+            (new Notifiable)->notify(new AttackDetected($event->log));
+        } catch (Throwable $e) {
             report($e);
         }
-    }
-
-    protected function getNotifiableClass()
-    {
-        return new class() {
-            use Notifiable;
-
-            public function routeNotificationForMail()
-            {
-                return config('firewall.notifications.mail.to');
-            }
-
-            public function routeNotificationForSlack()
-            {
-                return config('firewall.notifications.slack.to');
-            }
-
-            public function getKey()
-            {
-                return 1;
-            }
-        };
     }
 }
