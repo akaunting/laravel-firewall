247390f1e98e43a79d8deef8e48472de7d0b5717
Denis Duliçi <denisdulici@gmail.com>
2021-12-20 10:33:17 +0300

    formatting

--- src/Exceptions/AccessDenied.php
+++ src/Exceptions/AccessDenied.php
@@ -4,7 +4,7 @@ namespace Akaunting\Firewall\Exceptions;
 
 use Exception;
 
-class FirewallBlockedException extends Exception
+class AccessDenied extends Exception
 {
     // Empty implementation as a stub for your own exception
 }
