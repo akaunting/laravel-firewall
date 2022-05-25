d0186248542cbdf686d1ffcd6dc4733d3cfb8cc1
MAL Langbridge <ml@townswebarchiving.com>
2022-05-25 12:14:13 +0100

    Patched service provider to work with Laravel 7, which requires an array as second parameter to middlewareGroup(), when it is null before config is cached.

--- src/Provider.php
+++ src/Provider.php
@@ -57,7 +57,7 @@ class Provider extends ServiceProvider
      */
     public function registerMiddleware($router)
     {
-        $router->middlewareGroup('firewall.all', config('firewall.all_middleware'));
+        $router->middlewareGroup('firewall.all', config('firewall.all_middleware') ?? []);
         $router->aliasMiddleware('firewall.agent', 'Akaunting\Firewall\Middleware\Agent');
         $router->aliasMiddleware('firewall.bot', 'Akaunting\Firewall\Middleware\Bot');
         $router->aliasMiddleware('firewall.ip', 'Akaunting\Firewall\Middleware\Ip');
