16f7db92042d1af7ac8c2b3ad9207ed5a77ebe0d
Denis Duliçi <denisdulici@gmail.com>
2023-03-20 22:48:38 +0300

    fixed #57 #58

--- tests/Feature/WhitelistTest.php
+++ tests/Feature/WhitelistTest.php
@@ -3,7 +3,6 @@
 namespace Akaunting\Firewall\Tests\Feature;
 
 use Akaunting\Firewall\Middleware\Whitelist;
-use Akaunting\Firewall\Models\Ip as Model;
 use Akaunting\Firewall\Tests\TestCase;
 
 class WhitelistTest extends TestCase
@@ -11,6 +10,13 @@ class WhitelistTest extends TestCase
     public function testShouldAllow()
     {
         config(['firewall.whitelist' => ['127.0.0.0/24']]);
+
+        $this->assertEquals('next', (new Whitelist())->handle($this->app->request, $this->getNextClosure()));
+    }
+
+    public function testShouldAllowMultiple()
+    {
+        config(['firewall.whitelist' => ['127.0.0.0/24', '127.0.0.1']]);
 
         $this->assertEquals('next', (new Whitelist())->handle($this->app->request, $this->getNextClosure()));
     }
